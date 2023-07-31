77dd82703524c54ee570f4871da2cbc78fc121c5
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-31 17:26:48 -0500

    feat: define colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        neutral: "hsl(var(--neutral))",
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))"
+        }
+      }
+    },
   },
   plugins: [],
 }
