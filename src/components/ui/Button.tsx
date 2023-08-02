79fda942b047cde82b372041466fa2c64135687e
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-02 09:55:21 -0500

    fix: outline style

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,16 @@
 import { cn } from "@/lib/utils"
-import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 import { forwardRef } from "react"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-sm text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:bg-neutral disabled:text-neutral-foreground rounded-lg gap-1",
+  "inline-flex items-center justify-center rounded-sm text-sm font-bold transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:bg-neutral disabled:text-neutral-foreground rounded-lg gap-1",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/80",
         accent: "bg-accent text-accent-foreground hover:bg-accent/80",
-        outline: "border border-primary disabled:border-neutral disabled:bg-background text-primary bg-background hover:bg-background/80",
-        "outline-accent": "border border-accent disabled:border-neutral disabled:bg-background text-accent bg-background hover:bg-background/80",
+        outline: "border-2 border-primary disabled:border-neutral disabled:bg-background text-primary bg-background hover:bg-primary/30 active:bg-primary/60 active:text-background",
+        "outline-accent": "border-2 border-accent disabled:border-neutral disabled:bg-background text-accent bg-background hover:bg-accent/30 active:bg-accent/60 active:text-background",
       },
       size: {
         default: "h-9 px-4 py-2",
@@ -27,7 +26,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {}
+  VariantProps<typeof buttonVariants> { }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, ...props }, ref) => {
